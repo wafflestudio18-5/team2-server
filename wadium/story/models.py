34b8259c07e8bd5c08fdf706aed04fc576db05fa
symoon <mrn538@snu.ac.kr>
2020-11-27 20:40:17 +0900

    Create story model

--- wadium/story/models.py
+++ wadium/story/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+# https://docs.djangoproject.com/en/3.1/ref/models/fields/#django.db.models.FileField.storage
+class Story(models.Model):
+    writer_id = models.ForeignKey(User, null=False, related_name='story', on_delete=models.CASCADE)
+    title = models.CharField(max_length=100, null=False)
+    subtitle = models.CharField(max_length=100, null=True)
+    featured_image = models.ImageField(upload_to='/file/path/')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    published = models.BooleanField(null=False, default=False)
+    published_at = models.DateTimeField(null=True)
+
+    # uid  = models.CharField(max_length=100, null=False)
+    # can't understand the role of uid
+
+
