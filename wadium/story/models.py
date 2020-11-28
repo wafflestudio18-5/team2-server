fa7e4c7e275e34f8daf8201a3fd2673d5e15ad8e
symoon <mrn538@snu.ac.kr>
2020-11-28 15:36:44 +0900

    Apply comments on story model

--- wadium/story/models.py
+++ wadium/story/models.py
@@ -2,18 +2,15 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 
-# https://docs.djangoproject.com/en/3.1/ref/models/fields/#django.db.models.FileField.storage
 class Story(models.Model):
-    writer_id = models.ForeignKey(User, null=False, related_name='story', on_delete=models.CASCADE)
-    title = models.CharField(max_length=100, null=False)
-    subtitle = models.CharField(max_length=100, null=True)
+    uid  = models.CharField(max_length=100, null=False)
+    writer = models.ForeignKey(User, null=False, related_name='stories', on_delete=models.CASCADE)
+    title = models.CharField(max_length=100, blank=False)
+    subtitle = models.CharField(max_length=200, blank=True)
     featured_image = models.ImageField(upload_to='/file/path/')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     published = models.BooleanField(null=False, default=False)
     published_at = models.DateTimeField(null=True)
-
-    # uid  = models.CharField(max_length=100, null=False)
-    # can't understand the role of uid
 
 
