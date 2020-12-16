3f8d348fd07175985a968f4f8bf0a4bff6c8063b
Sanggyu Lee <8325289+gyusang@users.noreply.github.com>
2020-12-17 02:07:15 +0900

    Add UserProfile, Email Auth, Social Auth models

--- wadium/user/models.py
+++ wadium/user/models.py
@@ -1,3 +1,66 @@
 from django.db import models
+from django.contrib.auth.models import User
+import secrets
+from django.utils import timezone
 
-# Create your models here.
+
+class EmailAddress(models.Model):
+    email = models.EmailField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='emails', null=True)
+    primary = models.BooleanField(default=False)  # 이메일 로그인에 사용되는 이메일이 primary 이메일.
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['user', 'primary'])
+        ]
+
+    def __str__(self):
+        return self.email
+
+
+def generate_token():
+    return secrets.token_hex(6)
+
+
+class EmailAuth(models.Model):
+    email_address = models.ForeignKey(EmailAddress, related_name='auths', on_delete=models.CASCADE)
+    expires_at = models.DateTimeField(null=True)  # set when email is sent
+    valid = models.BooleanField(default=True)
+
+    # 이메일로 전송된 토큰인 경우: True
+    # api 요청으로 전송된 토큰인 경우: False
+    # signup CHECK 요청은 is_email_token이 True인 경우에만 처리됨.
+    is_email_token = models.BooleanField(default=False)
+
+    # May raise IntegrityError when token is not unique for <0.000001 probability.
+    token = models.CharField(max_length=12, unique=True, default=generate_token)
+
+    @property
+    def expired(self):
+        return self.expires_at <= timezone.now()
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=300)
+    profile_image = models.URLField(max_length=300)
+    bio = models.CharField(max_length=140)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class UserSocial(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    email_address = models.OneToOneField(EmailAddress, on_delete=models.CASCADE)
+    oauth_token = models.CharField(max_length=100)  # This is unique, but no need to validate.
+
+    class Meta:
+        abstract = True
+
+
+class UserGoogle(UserSocial):
+    google_sub = models.CharField(max_length=255, unique=True)
+
+
+class UserFacebook(UserSocial):
+    facebook_id = models.CharField(max_length=100, unique=True)
--- wadium/user/tests.py
+++ wadium/user/tests.py
@@ -1,3 +1,36 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from .models import EmailAuth, generate_token, EmailAddress
+from django.utils import timezone
+import datetime
 
-# Create your tests here.
+
+class EmailAuthTestCase(SimpleTestCase):
+    def test_generate_token(self):
+        valid_chars = '0123456789abcdef'
+        token = generate_token()
+        self.assertEqual(len(token), 12)
+        self.assertLessEqual(set(token), set(valid_chars))
+
+    def test_default_token(self):
+        valid_chars = '0123456789abcdef'
+        email_auth = EmailAuth()
+        token = email_auth.token
+        self.assertEqual(len(token), 12)
+        self.assertLessEqual(set(token), set(valid_chars))
+
+    def test_expired_false(self):
+        email_auth = EmailAuth()
+        email_auth.expires_at = timezone.now() + datetime.timedelta(minutes=1)
+        self.assertFalse(email_auth.expired)
+
+    def test_expired_true(self):
+        email_auth = EmailAuth()
+        email_auth.expires_at = timezone.now() + datetime.timedelta(minutes=-1)
+        self.assertTrue(email_auth.expired)
+
+
+class EmailAddressTestCase(SimpleTestCase):
+    def test_email_address_str(self):
+        email = 'test@example.com'
+        email_address = EmailAddress(email=email)
+        self.assertEqual(str(email_address), email)
