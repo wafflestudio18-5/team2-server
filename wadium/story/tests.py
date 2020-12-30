06baa08a07e875fbd3092910cada8f99a1159526
symoon <mrn538@snu.ac.kr>
2020-12-31 03:02:42 +0900

    Import test libraries

--- wadium/story/tests.py
+++ wadium/story/tests.py
@@ -1,3 +1,8 @@
-from django.test import TestCase
-
-# Create your tests here.
+from django.test import SimpleTestCase, TestCase, Client
+from .models import Story
+from django.contrib.auth.models import User
+from django.utils import timezone
+import datetime
+from rest_framework.authtoken.models import Token
+from rest_framework import status
+import json
