250f0fb04b3710a47e46a87275836f47fccb8fd7
symoon <mrn538@snu.ac.kr>
2021-01-01 23:39:34 +0900

    Make testcase class

--- wadium/story/tests.py
+++ wadium/story/tests.py
@@ -1,12 +1,13 @@
-from django.test import SimpleTestCase, TestCase, Client
-from .models import Story
-from django.contrib.auth.models import User
+from django.test import TestCase, Client
 from django.utils import timezone
-import datetime
 from rest_framework.authtoken.models import Token
 from rest_framework import status
 import json
+import datetime
 
-class PostStoryTestCase(SimpleTestCase):
-    def setUp(self):
-        return 
+from .models import Story
+from django.contrib.auth.models import User
+
+class PostStoryTestCase(TestCase):
+    client = Client()
+    reset_sequences = True
