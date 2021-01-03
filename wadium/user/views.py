8d1c23ae98252d457bbe0a9f133206200208a1cd
Sanggyu Lee <8325289+gyusang@users.noreply.github.com>
2021-01-04 00:29:39 +0900

    modify user list()

--- wadium/user/views.py
+++ wadium/user/views.py
@@ -4,7 +4,8 @@ from django.db import transaction
 from django.utils import timezone
 from django.shortcuts import get_object_or_404
 
-from .serializers import UserSerializer, UserLoginSerializer, UserSelfSerializer, UserSocialSerializer, MyStorySerializer, UserStorySerializer
+from .serializers import UserSerializer, UserLoginSerializer, UserSelfSerializer, UserSocialSerializer, \
+    MyStorySerializer, UserStorySerializer
 from .models import EmailAddress, EmailAuth, UserProfile
 from .permissions import UserAccessPermission
 from story.paginators import StoryPagination
@@ -120,8 +121,14 @@ class UserViewSet(viewsets.GenericViewSet):
         return Response(status=status.HTTP_204_NO_CONTENT)
 
     def list(self, request):
-        username = request.query_params.get('username')
-        users = self.get_queryset().filter(username__icontains=username).select_related('userprofile').prefetch_related('emails')
+        username = request.query_params.get('username', '')
+        if not username:
+            return Response({
+                'error': 'username query is required.'
+            }, status=status.HTTP_400_BAD_REQUEST)
+        users = self.get_queryset().filter(username__icontains=username) \
+            .select_related('userprofile') \
+            .prefetch_related('emails')
         if users.count() == 0:
             return Response(status=status.HTTP_404_NOT_FOUND)
         else:
@@ -132,10 +139,10 @@ class UserViewSet(viewsets.GenericViewSet):
         user = request.user if pk == 'me' else self.get_object()
         if user.is_authenticated:
             return Response(self.get_serializer(user).data)
-        else :
+        else:
             return Response(status=status.HTTP_404_NOT_FOUND)
 
-    #자신의 정보 확인
+    # 자신의 정보 확인
     def retrieve(self, request, pk=None):
         if pk != 'me':
             return Response({"error": "Can't show other user's information"}, status=status.HTTP_403_FORBIDDEN)
@@ -144,7 +151,7 @@ class UserViewSet(viewsets.GenericViewSet):
         serializer = self.get_serializer(profile)
         return Response(serializer.data)
 
-    #자신의 정보 수정
+    # 자신의 정보 수정
     def update(self, request, pk=None):
         if pk != 'me':
             return Response({"error": "Can't update other user's information"}, status=status.HTTP_403_FORBIDDEN)
@@ -208,4 +215,3 @@ class UserViewSet(viewsets.GenericViewSet):
         assert page is not None
         serializer = self.get_serializer(page, many=True)
         return self.get_paginated_response(serializer.data)
-
