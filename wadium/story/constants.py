cddedbf08b3bc4f69b378f5faf8df1b98151c29e
symoon <mrn538@snu.ac.kr>
2021-01-06 05:33:49 +0900

    Add test post comment

---
+++ wadium/story/constants.py
@@ -0,0 +1,26 @@
+body_example = [
+        [
+            {
+                "type": "paragraph",
+                "detail": {
+                    "content": "Wadium",
+                    "emphasizing": "large"
+                }
+            },
+            {
+                "type": "paragraph",
+                "detail": {
+                    "content": "Normal <em>hello! <strong>asdbasdnb</strong>asdbpoiahsb</em>",
+                    "emphasizing": "normal"
+                }
+            },
+            {
+                "type": "image",
+                "detail": {
+                    "size": "normal",
+                    "imgsrc": "https://wadium.shop/image/",
+                    "content": "image caption"
+                }
+            }
+        ]
+    ] 
