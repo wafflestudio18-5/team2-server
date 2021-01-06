a1b0b5747b67aa963a0f0c99241d97ca5c988980
Sanggyu Lee <8325289+gyusang@users.noreply.github.com>
2021-01-07 00:58:29 +0900

    Feature/#39 (#40)
    * Add google social login

---
+++ wadium/user/providers/google/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from .views import oauth2_callback, oauth2_login
+
+urlpatterns = [
+    path("google/login/", oauth2_login, name='google_no_redirect_login'),
+    path("google/login/callback/", oauth2_callback, name='google_no_redirect_callback')
+]
