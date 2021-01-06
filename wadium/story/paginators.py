81fcce3b0a49d1a42fb1f03bb766f1a72ca7691e
symoon <mrn538@snu.ac.kr>
2021-01-07 02:52:13 +0900

    Add CommentPagination class

--- wadium/story/paginators.py
+++ wadium/story/paginators.py
@@ -15,3 +15,17 @@ class StoryPagination(PageNumberPagination):
             'previous': self.get_previous_link(),
             'stories': data
         }
+
+class CommentPagination(PageNumberPagination):
+    page_size = 10
+
+    def get_paginated_response(self, data):
+        return Response(self.get_paginated_data(data))
+
+    def get_paginated_data(self, data):
+        return {
+            'count': self.page.paginator.count,
+            'next': self.get_next_link(),
+            'previous': self.get_previous_link(),
+            'comments': data
+        }
